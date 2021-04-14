1cc7bf211bb94675eda71142faa81a08c1c5086c
DaniloNunesBR <45714647+DaniloNunesBR@users.noreply.github.com>
2021-04-14 13:04:24 -0300

    Update plagiumdetect.py

--- plagiumdetect.py
+++ plagiumdetect.py
@@ -1,17 +1,3 @@
-"""
-Esse programa detecta o texto que possui maior similaridade com o texto principal !
-
-1. Primeiro você insere o texto principal a ser comparado.
-2. Depois você insere todos os textos que serão comparados com o texto principal.
-3. No final ele retorna qual dos textos possui maior similaridade com o texto principal
-
-* O programa é estruturado e possui funções separadas, e pode ser facilmente editado
-para diversas funcionalidades, como por exemplo: Detectar plágio.
-
-Obs: Se nenhum texto tiver similaridade, ele retornará que o texto 1 é o mais similar,
-por isso, se ele retornar o texto 1, é necessário verificar na prática se houve plágio.
-"""
-
 import re
 
 def le_assinatura():
